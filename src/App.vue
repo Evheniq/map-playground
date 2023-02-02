fd6ace74b4afc8651490653353e3b732323c4d49
Evheniq <qundola@gmail.com>
2023-02-02 02:37:06 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+
+  <div id="map">
+
+  </div>
+
+</template>
+
+<script setup>
+
+// import {reactive, ref} from "vue";
+//
+// let mapsToSelect = reactive({
+//   openStreet: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+//   stamen: 'http://a.tile.stamen.com/watercolor/{Z}/{X}/{Y}.jpg',
+//   NASA: 'https://tileserver.maptiler.com/nasa/{Z}/{X}/{Y}.jpg',
+// })
+//
+// let selectedMap = ref("openStreet")
+//
+// let map = L.map('app').setView([51.505, -0.09], 13);
+//
+// L.tileLayer(mapsToSelect[selectedMap.value], {
+//   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// }).addTo(map);
+</script>
+
+<style scoped>
+
+
+</style>
